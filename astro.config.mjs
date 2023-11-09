b71b2c798f5b9007713da015e0687e06b165849a
Romeo <chen.zitong@qq.com>
2023-11-09 16:15:52 +0800

    使用 local image

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import vue from "@astrojs/vue";
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,24 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+  // Type-check frontmatter using a schema
+  schema: ({ image }) => z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: image(),
+  }),
 });
 
-export const collections = { blog };
+const product = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    productName: z.string(),
+    price: z.string(),
+    createDate: z.coerce.date(),
+    productImage: z.string().optional(),
+  }),
+});
+
+export const collections = { blog, product };
